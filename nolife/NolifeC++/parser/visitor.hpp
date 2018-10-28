920bab751e9895e50dbdc51322a619a61d368a86
Josh Chandler <j5d1c7@gmail.com>
2018-10-28 12:33:47 -0400

    fixed printer, added all required includes

--- nolife/NolifeC++/parser/visitor.hpp
+++ nolife/NolifeC++/parser/visitor.hpp
@@ -16,20 +16,52 @@ namespace ast {
     class Float;
     class Character;
     class Void;
+    class ArrayAccess;
+    class Array;
+    class Assignment;
+    class Call;
+    class CaseLabels;
+    class Case;
+    class Clause;
+    class Constant;
+    class Expression;
+    class If;
+    class Procedure;
+    class Return;
+    class Statement;
+    class Variable;
+    class While;
+    class Write;
 }
 
 class Visitor {
     public:
-        virtual void visit(ast::Base* b) {};
-        virtual void visit(ast::Program* p) {};
-        virtual void visit(ast::Declaration* d) {};
-        virtual void visit(ast::CompoundStatement* cs) {};
-        virtual void visit(ast::Parameters* p) {};
-        virtual void visit(ast::Symbol* s) {};
-        virtual void visit(ast::Type* t) {};
-        virtual void visit(ast::Integer* i) {};
-        virtual void visit(ast::Float* f) {};
-        virtual void visit(ast::Character* c) {};
-        virtual void visit(ast::Void* v) {};
+        virtual void visit(ast::Base* b) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Program* p) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Declaration* d) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::CompoundStatement* cs) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Parameters* p) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Symbol* s) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Type* t) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Integer* i) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Float* f) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Character* c) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Void* v) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::ArrayAccess* aa) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Array* a) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Assignment* a) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Call* c) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::CaseLabels* cl) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Case* c) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Clause* c) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Constant* c) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Expression* e) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::If* i) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Procedure* p) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Return* r) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Statement* s) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Variable* v) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::While* w) {std::cout << "this is in the visitor base class.\n"; };
+        virtual void visit(ast::Write* w) {std::cout << "this is in the visitor base class.\n"; };
     private: 
 };
--- nolife/NolifeC++/parser/visitortypechecker.hpp
+++ nolife/NolifeC++/parser/visitortypechecker.hpp
@@ -4,7 +4,31 @@
 #include <string>
 
 #include "visitor.hpp"
+
+#include "astnode.hpp"
+#include "astarrayaccessnode.hpp"
+#include "astarraynode.hpp"
+#include "astassignnode.hpp"
+#include "astcallnode.hpp"
+#include "astcaselabelsnode.hpp"
+#include "astcasenode.hpp"
+#include "astclausenode.hpp"
+#include "astcompoundstmtnode.hpp"
+#include "astconstantnode.hpp"
+#include "astdeclnode.hpp"
+#include "astexpressionnode.hpp"
+#include "astifnode.hpp"
+#include "astparamnode.hpp"
+#include "astprocnode.hpp"
+#include "astprognode.hpp"
+#include "astreadnode.hpp"
+#include "astreturnnode.hpp"
+#include "aststmtnode.hpp"
+#include "astsymnode.hpp"
 #include "asttypenode.hpp"
+#include "astvariablenode.hpp"
+#include "astwhilenode.hpp"
+#include "astwritenode.hpp"
 
 class SymbolInfo {
     public:
@@ -20,14 +44,6 @@ class SymbolInfo {
         bool isArray;
         bool isProcedure;
 };
-
-namespace ast {
-    class Program;
-    class Declaration;
-    class CompoundStatement;
-    class Parameters;
-    class Symbol;
-}
 
 class TypeCheckVisitor : public Visitor {
     public:
